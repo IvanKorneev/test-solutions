c066489cf82d4678f159e2fb8d4015935a0087b0
IvanKorneev <ivan.kornieiev@gmail.com>
2022-11-25 21:39:29 +0200

    add post data

--- src/components/UsersForm/users-form.tsx
+++ src/components/UsersForm/users-form.tsx
@@ -11,6 +11,10 @@ import {
     validateUserPhone,
     validateUserWebsite
 } from "./input-form-validation";
+import Loader from "../Loader/loader";
+import Error from "../Error/error";
+import {useTypedSelector} from "../Hooks/useTypedSelector";
+import {useActions} from "../Hooks/useActions";
 
 interface MyFormValues {
     name: string;
@@ -18,70 +22,85 @@ interface MyFormValues {
     phone: string;
     company: string;
     website: string
-
 }
-
 
 
 export const UserForm: React.FC<{}> = () => {
     const initialValues: MyFormValues = {name: '', email: '', phone: '', company: '', website: ''};
+    const {error, loading} = useTypedSelector(state => state.user);
+    const {postUser} = useActions();
+
+    if (loading) {
+        return <Loader/>
+    }
+    if (error) {
+        return <Error/>
+    }
     return (
         <div>
             <h1>My Example</h1>
             <Formik
                 initialValues={initialValues}
                 onSubmit={(values, actions) => {
-                    console.log({values, actions});
+                    console.log(values);
+                    postUser({
+                        'id': Date.now() + Math.random(),
+                        'name': values.name,
+                        'email': values.email,
+                        'phone': values.phone,
+                        'company': values.company,
+                        'website': values.website,
+                    });
                     actions.setSubmitting(false);
                     actions.resetForm();
                 }}
             >
-                {({ errors, touched, isValidating }) => (
-                <Form>
-                    <label htmlFor="name">Name</label>
-                    <Field
-                        id="name"
-                        name="name"
-                        placeholder="name"
-                        validate={validateUserName}
-                    />
-                    {touched.name && errors.name && <div>{errors.name}</div>}
+                {({errors, touched, isValidating}) => (
+                    <Form>
+                        <label htmlFor="name">Name</label>
+                        <Field
+                            id="name"
+                            name="name"
+                            placeholder="name"
+                            validate={validateUserName}
+                        />
+                        {touched.name && errors.name && <div>{errors.name}</div>}
 
-                    <label htmlFor="email">Email</label>
-                    <Field
-                        id="email"
-                        name="email"
-                        placeholder="email"
-                        validate={validateEmail}
-                    />
-                    {errors.email && touched.email && <div>{errors.email}</div>}
+                        <label htmlFor="email">Email</label>
+                        <Field
+                            id="email"
+                            name="email"
+                            placeholder="email"
+                            validate={validateEmail}
+                        />
+                        {errors.email && touched.email && <div>{errors.email}</div>}
 
-                    <label htmlFor="phone">Phone</label>
-                    <Field id="phone"
-                           name="phone"
-                           placeholder="phone"
-                           validate={validateUserPhone}
-                    />
-                    {errors.phone && touched.phone && <div>{errors.phone}</div>}
+                        <label htmlFor="phone">Phone</label>
+                        <Field id="phone"
+                               name="phone"
+                               placeholder="phone"
+                               validate={validateUserPhone}
+                        />
+                        {errors.phone && touched.phone && <div>{errors.phone}</div>}
 
-                    <label htmlFor="company">Company</label>
-                    <Field id="company"
-                           name="company"
-                           placeholder="company"
-                           validate={validateUserCompany}
-                    />
-                    {errors.company && touched.company && <div>{errors.company}</div>}
+                        <label htmlFor="company">Company</label>
+                        <Field id="company"
+                               name="company"
+                               placeholder="company"
+                               validate={validateUserCompany}
+                        />
+                        {errors.company && touched.company && <div>{errors.company}</div>}
 
-                    <label htmlFor="website">Website</label>
-                    <Field id="website"
-                           name="website"
-                           placeholder="website"
-                           validate={validateUserWebsite}
-                    />
-                    {errors.website && touched.website && <div>{errors.website}</div>}
-                    <button type="submit">Submit</button>
-                </Form>
-                    )}
+                        <label htmlFor="website">Website</label>
+                        <Field id="website"
+                               name="website"
+                               placeholder="website"
+                               validate={validateUserWebsite}
+                        />
+                        {errors.website && touched.website && <div>{errors.website}</div>}
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
             </Formik>
         </div>
     );
