fe0496fc0e0e6220d87e40ec19b3ae9a63e1473e
IvanKorneev <ivan.kornieiev@gmail.com>
2022-11-25 10:31:48 +0200

    add custom hooks for getting state

--- src/components/UsersTable/user-table.tsx
+++ src/components/UsersTable/user-table.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import {useSelector} from "react-redux";
+import {useTypedSelector} from "../Hooks/useTypedSelector";
 
 const UserTable: React.FC = () => {
-    const state = useSelector(state => state);
-    console.log(state)
+    const {users,error,loading} = useTypedSelector(state => state.users);
     return (
         <div>
 
