f6f9b6fca6b5293fde0c9c397a39f3e83dc9dcc8
IvanKorneev <ivan.kornieiev@gmail.com>
2022-11-26 19:24:04 +0200

    add style error

--- src/components/Error/error.tsx
+++ src/components/Error/error.tsx
@@ -1,14 +1,18 @@
+import './error.sass';
 const Error = () => {
 
     return (
-        <div className='error'>
-            <h1 className='global-title'>OOPS!</h1>
-            <p>Page not found crying face emoji </p>
-            <img
-                src='https://www.garageclothing.com/on/demandware.static/-/Library-Sites-global_library_ca/default/dw209f52f1/Icons-Garage/crying_emoji.png' alt='img'/>
-            <p>Come back later or go to our homepage to find what you're looking for.</p>
+        <div className='error-container'>
+            <div className='error'>
+                <h1 className='global-title'>OOPS!</h1>
+                <p>Page not found</p>
+                <img
+                    src='https://www.garageclothing.com/on/demandware.static/-/Library-Sites-global_library_ca/default/dw209f52f1/Icons-Garage/crying_emoji.png' alt='img'/>
+                <p>Come back later or go to our homepage to find what you're looking for.</p>
+            </div>
         </div>
-    )
-}
+
+    );
+};
 
 export default Error;
